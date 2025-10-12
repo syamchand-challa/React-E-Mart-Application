04a845319ab4b2b053ffa8ddba245c396da6972f
challa-syamchand <challasyamchand26@gmail.com>
2025-10-12 12:13:45 +0530

    Setup complete React E-Commerce project

---
+++ src/stores/data/mobiles.js
@@ -0,0 +1,122 @@
+export const mobileData = [{
+
+        "id": "1",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/1.png",
+        "company": "Oneplus",
+        "model": "Oneplus Nord Ce5 5g",
+        "price": "23,499",
+        "category": "Electronics",
+        "description": "The latest iPhone model with advanced camera capabilities and powerful performance."
+    },
+    {
+        "id": "2",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/2.jpg",
+        "company": "Oppo",
+        "model": "Oppo A78 5G",
+        "price": "15000",
+        "category": "Electronics",
+        "description": "A foldable smartphone with a large display and multitasking features."
+    },
+    {
+        "id": "3",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/3.jpg",
+        "company": "Oneplus",
+        "model": "Oneplus Nord 3 5g",
+        "price": "35000",
+        "category": "Electronics",
+        "description": "A Google Pixel phone with exceptional camera quality and the latest Android features."
+    },
+    {
+        "id": "4",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/4.jpg",
+        "company": "Samsung",
+        "model": "Samsung GALAXY M13 5G",
+        "price": "15000",
+        "category": "Electronics",
+        "description": "A flagship OnePlus phone known for its smooth performance and fast charging."
+    },
+    {
+        "id": "5",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/5.jpg",
+        "company": "Vivo",
+        "model": "Vivo Y 16",
+        "price": "13000",
+        "category": "Electronics",
+        "description": "A high-spec Xiaomi phone with a versatile camera system and powerful hardware."
+    },
+    {
+        "id": "6",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/6.jpg",
+        "company": "Readmi",
+        "model": "Redmi Note 11s",
+        "price": "16000",
+        "category": "Electronics",
+        "description": "A Sony Xperia phone with a 4K display and pro-level camera features."
+    },
+    {
+        "id": "7",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/7.jpg",
+        "company": "Tecno Mobile",
+        "model": "Tecno Spark 10 5G ",
+        "price": "1000",
+        "category": "Electronics",
+        "description": "An LG smartphone with AI-enhanced features and a sleek design."
+    },
+    {
+        "id": "8",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/8.jpg",
+        "company": "Jio Bharat",
+        "model": "Bharat",
+        "price": "1000",
+        "category": "Electronics",
+        "description": "A Motorola Edge phone with 5G capabilities and a high-refresh-rate display."
+    },
+    {
+        "id": "9",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/9.jpg",
+        "company": "Huawei",
+        "model": "P50 Pro",
+        "price": "1299.00",
+        "category": "Electronics",
+        "description": "A Huawei flagship phone with advanced camera technology and sleek aesthetics."
+    },
+    {
+        "id": "10",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/10.jpg",
+        "company": "Oppo",
+        "model": "Find X5 Pro",
+        "price": "899.50",
+        "category": "Electronics",
+        "description": "An Oppo Find X phone with innovative design and impressive camera features."
+    },
+    {
+        "id": "11",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/11.jpg",
+        "company": "Nokia",
+        "model": "8.4 5G",
+        "price": "549.00",
+        "category": "Electronics",
+        "description": "A Nokia smartphone with 5G connectivity and a focus on reliability."
+    },
+    {
+        "id": "12",
+        "product": "Mobile",
+        "image": "/assets/NewMobile/12.jpg",
+        "company": "Oppo",
+        "model": "Oppo Reno 10",
+        "price": "25000",
+        "category": "Electronics",
+        "description": "A Realme phone designed in collaboration with a renowned designer, featuring a unique aesthetic."
+    }
+]
